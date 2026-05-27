20fb0367586bdb8a982ccfd0f543947376e95949
James Carlson <jxxcarlson@gmail.com>
2026-05-27 06:58:59 -0400

    test: add placeholder for poller integration tests

---
+++ memos/__tests__/telegram-memo-poller.test.js
@@ -0,0 +1,8 @@
+// Note: Full integration testing is done manually in Task 7.
+// These are unit tests for parser/helper logic that can be extracted.
+
+describe('Polling Script Integration', () => {
+  test('placeholder: integration tests are manual in Task 7', () => {
+    expect(true).toBe(true);
+  });
+});

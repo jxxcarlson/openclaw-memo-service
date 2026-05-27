28d106f32733b65b508cc133efd9991f6b2d1fde
James Carlson <jxxcarlson@gmail.com>
2026-05-27 11:40:02 -0400

    feat: add ContentView with tab interface

--- MemoViewer/MemoViewer/Views/ContentView.swift
+++ MemoViewer/MemoViewer/Views/ContentView.swift
@@ -1,11 +1,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var memoFolder: MemoFolder
+
+    init(memosPath: URL, archivePath: URL) {
+        _memoFolder = StateObject(wrappedValue: MemoFolder(memosPath: memosPath, archivePath: archivePath))
+    }
+
     var body: some View {
-        Text("MemoViewer")
+        TabView {
+            CurrentMemosView(memoFolder: memoFolder)
+                .tabItem {
+                    Label("Current", systemImage: "doc.text")
+                }
+
+            ArchiveView(memoFolder: memoFolder)
+                .tabItem {
+                    Label("Archive", systemImage: "archivebox")
+                }
+        }
+        .frame(minWidth: 600, minHeight: 400)
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        let tempDir = FileManager.default.temporaryDirectory
+        ContentView(memosPath: tempDir, archivePath: tempDir)
+    }
 }

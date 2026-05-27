6ea8ba092f2ab2650fd0af5416887328d7419a5b
James Carlson <jxxcarlson@gmail.com>
2026-05-27 11:21:40 -0400

    project: remove over-scoped implementation, keep structure only

--- MemoViewer/MemoViewer/Views/ContentView.swift
+++ MemoViewer/MemoViewer/Views/ContentView.swift
@@ -2,16 +2,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Welcome to MemoViewer")
-                .font(.title)
-            Text("Your markdown memos viewer")
-                .font(.subtitle)
-        }
-        .padding()
+        Text("MemoViewer")
     }
 }
 
